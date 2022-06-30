1649f9f534ee43b74c64483b931c73d0eb538d9e
iksan <iksangame80@gmail.com>
2022-06-30 15:13:23 +0700

    membuat project movie-catalogue

---
+++ e2e/Liking_Movies.spec.js
@@ -0,0 +1,25 @@
+Feature('Liking Movies');
+
+Before(({ I }) => {
+  I.amOnPage('/#/like');
+});
+
+Scenario('test something', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+});
+
+Scenario('liking one movie', ({ I }) => {
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.movie__title a');
+  I.click(locate('.movie__title a').first());
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.movie-item');
+});
